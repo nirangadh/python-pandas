77ac7fb1532028adea0215e104be2350a0a70078
Niranga Shashi Dharmaratna <nirangadh@users.noreply.github.com>
2025-03-21 16:03:01 +0530

    Create pd-version.py

---
+++ pd-version.py
@@ -0,0 +1,6 @@
+# In Google Colab, Pandas is pre-installed. To upgrade or install a specific version, you can run:
+!pip install --upgrade pandas
+
+# Verify the installation
+import pandas as pd
+print("Pandas version:", pd.__version__)
